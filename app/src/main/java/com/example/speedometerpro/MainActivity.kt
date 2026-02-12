97e468168254d6b4f4adabdefa5830f23429fb71
arslanmalik1266 <mailk.arslan.ap@gmail.com>
2026-02-12 16:44:08 +0500

    initial

---
+++ app/src/main/java/com/example/speedometerpro/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.speedometerpro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.speedometerpro.customView.SpeedometerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        val speedometer = findViewById<SpeedometerView>(R.id.speedometer)
+
+        speedometer.setSpeed(120f)
+    }
+}
